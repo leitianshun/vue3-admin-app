10b64e7006ddad546adf0628402396695f8c6343
leitianshun <3117929324@qq.com>
2023-05-20 14:21:53 +0800

    feat: add auto-import vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueComponents from 'unplugin-vue-components/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    AutoImport({
+      imports: ['vue', 'pinia', 'vue-router'],
+      dirs:['src/stores']
+    }),
+    VueComponents({
+      dirs: ['src/components'],
+      resolvers: [],
+      types: [],
+    }),
+    vue()],
 })
