cca8d0130137e1e34b756ecfc84bfde6ea20212f
leitianshun <3117929324@qq.com>
2023-05-23 21:38:15 +0800

    feat: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -20,5 +21,11 @@ export default defineConfig({
     }),
     vue(),
     UnoCSS(),
+
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
 })
