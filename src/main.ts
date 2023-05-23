bd9ab7dfbd2040344e8daa1c1183cfd5b3a4f3c0
leitianshun <3117929324@qq.com>
2023-05-23 21:23:22 +0800

    feat: 配置element-plus语言国际化

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+
+// @ts-expect-error ts忽略下一行代码类型检查
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import './style.css'
 import App from './App.vue'
 import 'virtual:uno.css'
@@ -7,5 +11,8 @@ import { setRouter } from './router'
 const app = createApp(App)
 setPinia(app)
 setRouter(app)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
 // createApp(App ).mount('#app')
