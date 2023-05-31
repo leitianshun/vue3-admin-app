afb9094dae36004cdfafba06bb543304f6ad070c
leitianshun <3117929324@qq.com>
2023-05-31 12:56:43 +0800

    feat: 替换icon，以及统一管理logo，标题

--- src/layout/logo/index.vue
+++ src/layout/logo/index.vue
@@ -1,11 +1,12 @@
 <script setup lang='ts'>
+import setting from '@/setting'
 </script>
 
 <template>
   <div class="w-full h-15  flex items-center text-white justify-center">
-    <img src="@/assets/image/jntm.gif" class="w-10 h-10 rounded-full border-gray border-opacity-50 border-1 border-solid shadow-yellow-300 " alt="">
+    <img :src="setting.logo" class="w-10 h-10 rounded-full border-gray border-opacity-50 border-1 border-solid shadow-yellow-300 " alt="">
     <div class="ml-3">
-      小黑子甄选运营平台
+      {{ setting.title }}
     </div>
   </div>
 </template>
---
+++ src/setting.ts
@@ -0,0 +1,4 @@
+export default {
+  title: '小黑子甄选运营平台',
+  logo: '/src/assets/image/jntm.gif',
+}
