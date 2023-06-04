06ca7bc48eb4c86698264cdac1a7a8aa39cd85f5
leitianshun <3117929324@qq.com>
2023-06-04 17:12:48 +0800

    feat: 完成面包屑展示路由标题

--- src/layout/tabbar/breadcrumb/index.vue
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -3,6 +3,9 @@ const layoutStore = useLayoutStore()
 function changeIcon() {
   layoutStore.setCollapse()
 }
+
+const route = useRoute()
+console.log(route.matched)
 </script>
 
 <template>
@@ -12,8 +15,14 @@ function changeIcon() {
     </el-icon>
     <div class="ml-3">
       <el-breadcrumb separator-icon="ArrowRight">
-        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item v-for="(item, index) in route.matched" v-show="item.meta.title" :key="index" :to="item.path">
+          <div class="flex items-center">
+            <el-icon>
+              <component :is="item.meta.icon" class="w-4.5 h-4.5" />
+            </el-icon>
+            <span class="ml-1">{{ item.meta.title }}</span>
+          </div>
+        </el-breadcrumb-item>
       </el-breadcrumb>
     </div>
   </div>
