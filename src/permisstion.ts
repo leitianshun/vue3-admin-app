fb2a797908888af1998b798995b230a70a12a363
leitianshun <3117929324@qq.com>
2023-06-05 21:31:05 +0800

    fix: 处理token过期后，跳到登录页

--- src/permisstion.ts
+++ src/permisstion.ts
@@ -18,8 +18,9 @@ router.beforeEach(async (to: any, from: any, next: any) => {
           await useUserStore().userInfo() // 每次刷新，都需要重新获取动态路由,再次使用addRoute添加到路由表中。
           next({ ...to, replace: true }) // 防止刷新的时候是异步路由,有可能获取到用户信息、异步路由还没有加载完毕,出现空白的效果  hank方法
         }
-        catch (err) {
-          useUserStore().logout()
+        catch (err) { // 如果用户信息获取失败，有可能是token过期，此时跳转到的登录页，先退出登录成功后再跳登录页面。
+          await useUserStore().logout()
+          next({ path: '/login', query: { redirect: to.path } })
         }
       }
     }
