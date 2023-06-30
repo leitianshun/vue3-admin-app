b482afa38cb2782b0669d683092f9516d9934194
leitianshun <3117929324@qq.com>
2023-06-30 17:58:38 +0800

    feat: 完成添加属性的基本逻辑

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,8 +1,11 @@
 <script setup lang='ts'>
 // import type {category1Data} from '@/api/product/attr/type'
 
-function handleTradmark() {}
-function deleteTradmark() {}
+const show = ref<boolean>(true)
+function handleUpload(row: any) {
+  show.value = false
+}
+function handleDelete(row: any) {}
 const categoryStore = useCategoryStore()
 </script>
 
@@ -10,29 +13,34 @@ const categoryStore = useCategoryStore()
   <div>
     <Category />
     <el-card class="mt-3">
-      <!-- 通过判断是否有三级分类id 决定按钮的禁用  也可以写成 !categoryStore.category3Id 来判断 -->
-      <el-button icon="Plus" type="primary" :disabled="categoryStore.category3Id ? false : true">
-        添加平台属性
-      </el-button>
-      <el-table :data="categoryStore.attrListData" border height="calc(100vh - 300px)" class="mt-5">
-        <el-table-column type="index" width="80" align="center" label="序号" />
-        <el-table-column prop="attrName" label="属性名称" width="120" align="center" />
-        <el-table-column prop="attrValueList" label="属性值名称" align="center">
-          <template #default="{ row }">
-            <div>
-              <el-button v-for="item in row.attrValueList" :key="item.id" type="primary" size="small">
-                {{ item.valueName }}
-              </el-button>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="150" align="center">
-          <template #default="{ row }">
-            <el-button type="warning" icon="Edit" @click="handleTradmark(row)" />
-            <el-button type="danger" icon="Delete" @click="deleteTradmark(row)" />
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="show">
+        <!-- 通过判断是否有三级分类id 决定按钮的禁用  也可以写成 !categoryStore.category3Id 来判断 -->
+        <el-button icon="Plus" type="primary" :disabled="categoryStore.category3Id ? false : true" @click="show = false">
+          添加平台属性
+        </el-button>
+        <el-table :data="categoryStore.attrListData" border height="calc(100vh - 300px)" class="mt-5">
+          <el-table-column type="index" width="80" align="center" label="序号" />
+          <el-table-column prop="attrName" label="属性名称" width="120" align="center" />
+          <el-table-column prop="attrValueList" label="属性值名称" align="center">
+            <template #default="{ row }">
+              <div>
+                <el-button v-for="item in row.attrValueList" :key="item.id" type="primary" size="small">
+                  {{ item.valueName }}
+                </el-button>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="150" align="center">
+            <template #default="{ row }">
+              <el-button type="warning" icon="Edit" @click="handleUpload(row)" />
+              <el-button type="danger" icon="Delete" @click="handleDelete(row)" />
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div v-show="!show">
+        666
+      </div>
     </el-card>
   </div>
 </template>
