1eb59c0929816bf46f8254d88ab8a14c20269e61
leitianshun <3117929324@qq.com>
2023-07-28 21:38:32 +0800

    feat: 完成大屏顶部

--- src/views/screen/screen.vue
+++ src/views/screen/screen.vue
@@ -1,9 +1,50 @@
 <script setup lang='ts'>
+import Top from './top/index.vue'
+
+const screen = ref()
+onMounted(() => {
+  // const box: any = document.querySelector('.screen-box')
+  // box.style.transform = `scale(${getScale()})`
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+function getScale(w = 1920, h = 1080) {
+  const scaleWidth = window.innerWidth / w
+  const scaleHeight = window.innerHeight / h
+  return scaleWidth < scaleHeight ? scaleWidth : scaleHeight
+}
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
 </script>
 
 <template>
-  <div>screen</div>
+  <div class="box w-screen h-screen">
+    <div ref="screen" class="fixed left-1/2 top-1/2 transform -translate-x-1/2 screen-box  -translate-y-1/2 w-[1920px] h-[1080px] ">
+      <div class="w-full h-10 ">
+        <Top />
+      </div>
+      <div>中部</div>
+      <div class="flex">
+        <div class="flex-1">
+          左
+        </div>
+        <div class="flex-[2]">
+          中
+        </div>
+        <div class="flex-1">
+          右
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang='scss'>
+.box {
+  background: url('/src/views/screen/images/bg.png') no-repeat;
+  background-size: cover;
+  .screen-box{
+    transform-origin: left top;  //设置起点，这里就是根据原有位置进行移动，而不是根据屏幕现有位置移动
+  }
+}
 </style>
---
+++ src/views/screen/top/index.vue
@@ -0,0 +1,68 @@
+<script setup lang='ts'>
+import dayjs from 'dayjs'
+
+const timer = ref()
+const router = useRouter()
+const time = ref(dayjs().format('YYYY年MM月DD日 HH:mm:ss'))
+// const time = new Date().toLocaleString().replace(/\//g, '-')
+
+onMounted(() => {
+  timer.value = setInterval(() => {
+    time.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  }, 1000)
+})
+onBeforeUnmount(() => {
+  clearInterval(timer.value)
+})
+</script>
+
+<template>
+  <div class="flex  w-full h-full">
+    <div class="left flex-1 flex w-full justify-end">
+      <div class="left-btn w-40 h-full flex text-xl items-center justify-center cursor-pointer" @click="router.push('/')">
+        首页
+      </div>
+    </div>
+    <div class="center w-full h-20   text-3xl flex-[2]  flex items-center justify-center">
+      智慧旅游可视化大数据展示平台
+    </div>
+    <div class="right flex-1 flex w-full justify-between items-center ">
+      <div class="w-40 right-btn text-xl h-full flex items-center justify-center">
+        统计报告
+      </div>
+      <div class="mr-8 ">
+        当前时间  {{ time }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.left {
+  background: url('/src/views/screen/images/dataScreen-header-left-bg.png') no-repeat;
+  background-size: cover;
+
+  .left-btn {
+    background: url('/src/views/screen/images/dataScreen-header-btn-bg-l.png') no-repeat;
+    background-size: cover;
+    color: #29fcff;
+  }
+}
+
+.center {
+  background: url('/src/views/screen/images/dataScreen-header-center-bg.png') no-repeat;
+  background-size: cover;
+  color: #29fcff;
+}
+
+.right {
+  background: url('/src/views/screen/images/dataScreen-header-right-bg.png') no-repeat;
+  background-size: cover;
+    color: #29fcff;
+
+  .right-btn {
+    background: url('/src/views/screen/images/dataScreen-header-btn-bg-r.png') no-repeat;
+    background-size: cover;
+  }
+}
+</style>
