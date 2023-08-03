977f9fe73fbb4638503e415cd1f79c7ba9398356
leitianshun <3117929324@qq.com>
2023-08-03 13:12:41 +0800

    feat: 新增 pretter 代码格式化

--- src/views/screen/screen.vue
+++ src/views/screen/screen.vue
@@ -1,4 +1,4 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import Top from './components/top/index.vue'
 import Sex from './components/sex/index.vue'
 import Age from './components/age/index.vue'
@@ -24,34 +24,35 @@ window.onresize = () => {
 
 <template>
   <div class="box w-screen h-screen">
-    <div ref="screen" class="fixed left-1/2 top-1/2 transform -translate-x-1/2 screen-box  -translate-y-1/2 w-[1920px] h-[1080px] ">
-      <div class="w-full h-10 ">
+    <div
+      ref="screen"
+      class="fixed left-1/2 top-1/2 transform -translate-x-1/2 screen-box -translate-y-1/2 w-[1920px] h-[1080px]"
+    >
+      <div class="w-full h-10">
         <Top />
       </div>
-      <div class="flex ">
+      <div class="flex">
         <div class="flex-1 h-[1040px] mt-3 flex flex-col justify-around">
-          <Youke class="flex-[1.2] " />
+          <Youke class="flex-[1.2]" />
           <Sex class="flex-1 border my-5 bg-green-200" />
-          <Age class="flex-1  border bg-yellow-500" />
+          <Age class="flex-1 border bg-yellow-500" />
         </div>
         <div class="flex-[2] mt-11 flex flex-col">
-          <Map class="flex-[2.1]  " />
-          <Line class="flex-[1] bg-red-300 " />
+          <Map class="flex-[2.1]" />
+          <Line class="flex-[1] bg-red-300" />
         </div>
-        <div class="flex-1 bg-pink-500">
-          右
-        </div>
+        <div class="flex-1 bg-pink-500">右</div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .box {
   background: url('/src/views/screen/images/bg.png') no-repeat;
   background-size: cover;
-  .screen-box{
-    transform-origin: left top;  //设置起点，这里就是根据原有位置进行移动，而不是根据屏幕现有位置移动
+  .screen-box {
+    transform-origin: left top; //设置起点，这里就是根据原有位置进行移动，而不是根据屏幕现有位置移动
   }
 }
 </style>
